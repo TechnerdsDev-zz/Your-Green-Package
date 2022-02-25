1e96934d30bf29e3d5a2b2c0be70223e30438b49
WaleedKhan-21 <wk2841038@gmail.com>
2022-02-24 17:14:22 -0800

    toggle task

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,10 +1,22 @@
 import mainLogo from "../../assets/images/logo.png";
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 export default function Header() {
   const location = useLocation();
-
+  const [show, setShow] = useState(true);
+  const handleToggle = () => {
+    if (show === false) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+  useEffect(() => {
+    if (window.innerWidth < 767) {
+      setShow(false);
+    }
+  }, []);
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
@@ -18,41 +30,49 @@ export default function Header() {
                 <img src={mainLogo} alt="Logo" />
               </NavLink>
             </div>
-            <nav>
-              <div className="main_nav">
-                <ul>
-                  <li>
-                    <NavLink to="/" activeClassName="active">
-                      Home
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="about">About Us</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="faqs" activeClassName="active">
-                      FAQ
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="partners" activeClassName="active">
-                      Partners
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="contact">Contact Us</NavLink>
-                  </li>
+            {show ? (
+              <nav>
+                <div className="main_nav">
+                  <ul>
+                    <li>
+                      <NavLink to="/" activeClassName="active">
+                        Home
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="about">About Us</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="faqs" activeClassName="active">
+                        FAQ
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="partners" activeClassName="active">
+                        Partners
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="contact">Contact Us</NavLink>
+                    </li>
 
-                  <li>
-                    <NavLink to="garden-remedies-newton" className="custom_btn">
-                      Order Now
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-            <div class="burger">
-              <div class="strip burger-strip">
+                    <li>
+                      <NavLink
+                        to="garden-remedies-newton"
+                        className="custom_btn"
+                      >
+                        Order Now
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            ) : (
+              ""
+            )}
+
+            <div class={show ? "burger active" : "burger"}>
+              <div onClick={handleToggle} class="strip burger-strip">
                 <div></div>
                 <div></div>
                 <div></div>
