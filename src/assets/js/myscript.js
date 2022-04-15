5e7a3102e2fcfb01210db1b2329be5168c923570
Waleed Khan <waleedkhan@technerds.com>
2022-04-15 12:26:41 +0500

    changes

--- src/assets/js/myscript.js
+++ src/assets/js/myscript.js
@@ -1,78 +1,79 @@
-$(document).ready(function() {
-    $('#signIn').click(function() {
-        $('.signin').fadeIn();
-        $('body').addClass('hidden');
-    });
-    $('.menuIcon').click(function() {
-        $('.header_detail_inner').slideDown('visible');
-    });
+$(document).ready(function () {
+  $("#signIn").click(function () {
+    $(".signin").fadeIn();
+    $("body").addClass("hidden");
+  });
+  $(".menuIcon").click(function () {
+    $(".header_detail_inner").slideDown("visible");
+  });
 
-    $('.closeNav').click(function() {
-        $('.header_detail_inner').slideUp('visible');
-    });
-    $('#burgerBtn').click(function() {
-        $('body').toggleClass('hidden');
-        $('.wrapper').toggleClass('hidden');
-        $('.demo1 .nav').fadeToggle();
-    });
-    $('#menuToggle').click(function() {
-        $('.sidebar').toggleClass('show');
-        $('body').toggleClass('hidden');
-    });
-    $('.sidebar_nav ul li a, body').click(function() {
-        $('.sidebar').removeClass('show');
-        $('body').removeClass('hidden');
-        $('#menuToggle input').prop('checked', false);
-    });
-    $('.sidebar, #menuToggle').click(function(e) {
-        e.stopPropagation();
-    });
+  $(".closeNav").click(function () {
+    $(".header_detail_inner").slideUp("visible");
+  });
+  $("#burgerBtn").click(function () {
+    $("body").toggleClass("hidden");
+    $(".wrapper").toggleClass("hidden");
+    $(".demo1 .nav").fadeToggle();
+  });
+  $("#menuToggle").click(function () {
+    $(".sidebar").toggleClass("show");
+    $("body").toggleClass("hidden");
+  });
+  $(".sidebar_nav ul li a, body").click(function () {
+    $(".sidebar").removeClass("show");
+    $("body").removeClass("hidden");
+    $("#menuToggle input").prop("checked", false);
+  });
+  $(".sidebar, #menuToggle").click(function (e) {
+    e.stopPropagation();
+  });
 
-    var burgerBtn = document.getElementById('burgerBtn');
-    var mobile = document.getElementById('mobile');
-    var demo3 = document.getElementById('demo3');
+  var burgerBtn = document.getElementById("burgerBtn");
+  var mobile = document.getElementById("mobile");
+  var demo3 = document.getElementById("demo3");
 
-    burgerBtn.addEventListener('click', function() {
-        mobile.classList.toggle('navigation');
-    }, false);
+  burgerBtn.addEventListener(
+    "click",
+    function () {
+      mobile.classList.toggle("navigation");
+    },
+    false
+  );
 
-    demo3.addEventListener('click', function() {
-        demo3.classList.add('active');
-        demo1.classList.remove('active');
-        demo2.classList.remove('active');
-        mobile.classList.add('demo3');
-        mobile.classList.remove('demo1', 'demo2', 'navigation');
-    }, false);
+  demo3.addEventListener(
+    "click",
+    function () {
+      demo3.classList.add("active");
+      demo1.classList.remove("active");
+      demo2.classList.remove("active");
+      mobile.classList.add("demo3");
+      mobile.classList.remove("demo1", "demo2", "navigation");
+    },
+    false
+  );
 
-
-    animation
-    wow = new WOW({
-        animateClass: 'animated',
-        offset: 50,
-        callback: function(box) {
-            console.log("WOW: animating < " + box.tagName.toLowerCase() + ">")
-        }
-    });
-    wow.init();
-    document.getElementById('moar').onclick = function() {
-        var section = document.createElement('section');
-        section.className = 'section--purple wow fadeInDown';
-        this.parentNode.insertBefore(section, this);
-    };
-
+  animation;
+  wow = new WOW({
+    animateClass: "animated",
+    offset: 50,
+    callback: function (box) {
+      console.log("WOW: animating < " + box.tagName.toLowerCase() + ">");
+    },
+  });
+  wow.init();
+  document.getElementById("moar").onclick = function () {
+    var section = document.createElement("section");
+    section.className = "section--purple wow fadeInDown";
+    this.parentNode.insertBefore(section, this);
+  };
 });
-
-
-
-
-
 
 // sticky header
 
-$(window).scroll(function() {
-    if ($(window).scrollTop() >= 100) {
-        $('body').addClass('fixed_header');
-    } else {
-        $('body').removeClass('fixed_header');
-    }
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 100) {
+    $("body").addClass("fixed_header");
+  } else {
+    $("body").removeClass("fixed_header");
+  }
 });
--- src/components/elements/Blogs.js
+++ src/components/elements/Blogs.js
@@ -1,10 +1,8 @@
 import React from "react";
-// import blogOne from "../../assets/images/blog_one.png";
-// import blogTwo from "../../assets/images/blog_two.png";
-// import blogThree from "../../assets/images/blog_three.png";
+
 import postlist from "../../posts.json";
 import { Link } from "react-router-dom";
-// import Markdown from "react-markdown";
+
 import ReactMarkdown from "react-markdown";
 
 export default function Blogs() {
@@ -37,18 +35,10 @@ export default function Blogs() {
                               alt=""
                             />
                           )}
-                          {/* <img src={blogOne} alt="blog" /> */}
                         </figure>
                         <div className="blog_bottom">
                           <strong>
-                            <Link
-                              className="links"
-                              to={`post/${post.id}`}
-                              // to={{
-                              //   pathname: `post/${post.id}`,
-                              //   state: { posts: post.id },
-                              // }}
-                            >
+                            <Link className="links" to={`post/${post.id}`}>
                               {post.title}
                             </Link>
                             {/* Garden Remedies */}
@@ -58,67 +48,14 @@ export default function Blogs() {
                           </small>
                           <ReactMarkdown>{excerptList[i]}</ReactMarkdown>
 
-                          {/* <p> */}
-                          {/* <ReactMarkdown>{excerptList[i]}</ReactMarkdown> */}
-                          {/* {excerptList[i]}
-                            <Markdown
-                              source={excerptList[i]}
-                              escapeHtml={false}
-                            /> */}
-                          {/* Your Green Package is making Cannabis Deliveries in
-                            the Greater Boston, Newton, and Northamptonarea. */}
-                          {/* </p> */}
-                          {/* <a href={`/post/${post.id}`}> */}
-                          <Link
-                            className="custom_btn"
-                            to={`/post/${post.id}`}
-                            // to={{
-                            //   pathname: `post/${post.id}`,
-                            //   state: { posts: post.id },
-                            // }}
-                          >
+                          <Link className="custom_btn" to={`/post/${post.id}`}>
                             Read More
                           </Link>
-                          {/* </a> */}
                         </div>
                       </div>
                     </li>
                   );
                 })}
-              {/* <li>
-                      <div className="blog_main">
-                        <figure>
-                          <img src={blogTwo} alt="blog" />
-                        </figure>
-                        <div className="blog_bottom">
-                          <strong>Garden Remedies</strong>
-                          <p>
-                            Your Green Package is making Cannabis Deliveries in
-                            the Greater Boston, Newton, and Northamptonarea.
-                          </p>
-                          <a href="#" className="custom_btn">
-                            Read More
-                          </a>
-                        </div>
-                      </div>
-                    </li>
-                    <li>
-                      <div className="blog_main">
-                        <figure>
-                          <img src={blogThree} alt="blog" />
-                        </figure>
-                        <div className="blog_bottom">
-                          <strong>Garden Remedies</strong>
-                          <p>
-                            Your Green Package is making Cannabis Deliveries in
-                            the Greater Boston, Newton, and Northamptonarea.
-                          </p>
-                          <a href="#" className="custom_btn">
-                            Read More
-                          </a>
-                        </div>
-                      </div>
-                    </li> */}
             </ul>
           </div>
         </div>
